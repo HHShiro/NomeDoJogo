ca84bdd5e47d46c1c381cef3822ca3b5cd39265d
Lucas Aleixo <105569930+eumermo0007@users.noreply.github.com>
2026-02-16 16:01:16 -0300

    Inimigos e Cena
    Primeira Cena e inimigos criados

--- source/nome-do-jogo/scripts/enemy_basic01.gd
+++ source/nome-do-jogo/scripts/enemy_basic01.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-@export var movement_speed = 20.0
+@onready var movement_component: MovementComponent = $MovementComponent
 
-@onready var player = get_tree().get_first_node_in_gropu("player")
-
-func _physics_process(_delta):
+@onready var player = get_tree().get_first_node_in_group("player")
+func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction+movement_speed
-	move_and_slide()
+	movement_component.direction = direction
+	movement_component.tick(delta)
+	
