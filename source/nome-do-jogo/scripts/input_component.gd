a77ff7a0d3892cc01212b44e992fd17e73766d2c
RafaLBarros <95069113+HHShiro@users.noreply.github.com>
2026-02-16 14:06:45 -0300

    Criado InputComponent
    Criei o InputComponent e o mapa de atalhos para wasd, além do nó player inicial.

---
+++ source/nome-do-jogo/scripts/input_component.gd
@@ -0,0 +1,9 @@
+class_name InputComponent extends Node
+
+#Criando variável de direção
+var move_direction := Vector2.ZERO
+
+#Função que atualiza a cada tick dentro do jogo
+func update() -> void:
+	move_direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	print(move_direction)
