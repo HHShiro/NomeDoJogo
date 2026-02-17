efe1e629cd4a585b4e4abceb74589802ee7148a6
RafaLBarros <95069113+HHShiro@users.noreply.github.com>
2026-02-17 00:21:15 -0300

    Criado Hitbox, Hurtbox e HealthComponent
    Desenvolvi a health component e os componentes para atacar e receber ataques (hitbox e hurtbox)

--- source/nome-do-jogo/scripts/enemies/enemy_basic01.gd
+++ source/nome-do-jogo/scripts/enemies/enemy_basic01.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var movement_component: MovementComponent = $MovementComponent
+@onready var health_component: HealthComponent = $HealthComponent
 
 @onready var player = get_tree().get_first_node_in_group("player")
 func _physics_process(delta):
@@ -8,3 +9,13 @@ func _physics_process(delta):
 	movement_component.direction = direction
 	movement_component.tick(delta)
 	
+
+
+func _on_hurtbox_component_hurt(damage: Variant) -> void:
+	health_component.damage(damage)
+	print("Inimigo: ", health_component.current_health)
+
+
+func _on_health_component_died() -> void:
+	print("Murreu")
+	queue_free()
