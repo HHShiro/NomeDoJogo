bfc3f3896296572415d98d400685e29c939e8fc1
RafaLBarros <95069113+HHShiro@users.noreply.github.com>
2026-02-20 05:53:55 -0300

    Status, Itens e Evolução Feito
    Criado os Status, itens passivos e evolução de arma. Alguns polimentos ainda a serem feitos.

--- source/nome-do-jogo/scripts/player/SingleShot.gd
+++ source/nome-do-jogo/scripts/player/SingleShot.gd
@@ -10,6 +10,7 @@ func shoot(source,target, scene_tree):
 	projectile.position = source.position
 	projectile.damage = damage
 	projectile.speed = speed
+	projectile.source = source
 	projectile.direction = (target.position - source.position).normalized()
 	
 	scene_tree.current_scene.add_child(projectile)
@@ -18,6 +19,10 @@ func activate(source, target, scene_tree):
 	shoot(source, target, scene_tree)
 
 func upgrade_item(upgrade_index: int):
+	if max_level_reached() and passive_item_available:
+		slot.item = evolution
+		return
+	
 	if not is_upgradable():
 		return
 	
