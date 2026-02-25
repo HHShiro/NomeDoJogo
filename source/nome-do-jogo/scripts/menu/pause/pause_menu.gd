5c8c39a8819a2f8d67d16e1de6340774dcc4a473
Lucas Aleixo <105569930+eumermo0007@users.noreply.github.com>
2026-02-25 00:21:45 -0300

    Menu de Pause Completo

--- source/nome-do-jogo/scripts/menu/pause/pause_menu.gd
+++ source/nome-do-jogo/scripts/menu/pause/pause_menu.gd
@@ -1,10 +1,48 @@
 extends CanvasLayer
 
 
+func menu_pause():
+	$Options.hide()
+	$Back.hide()
+	$Filtro.show()
+	$Menu.show()
+	$Audio.hide()
+	$Controles.hide()
+
+
+func options():
+	$Back.show()
+	$Filtro.hide()
+	$Menu.hide()
+	$Options.show()
+	tween_pop($Options)
+
+func controles():
+	$Filtro.hide()
+	$Menu.hide()
+	$Options.hide()
+	$Audio.hide()
+	$Controles.show()
+	tween_pop($Controles)
+
+func audio():
+	$Filtro.hide()
+	$Menu.hide()
+	$Options.hide()
+	$Audio.show()
+	$Controles.hide()
+	tween_pop($Audio)
+
+func tween_pop(panel):
+	SoundManager.play_sfx(load("res://assets/sounds/sfx/button_menu_sound.wav"))
+	panel.scale = Vector2(0.85,0.85)
+	var tween = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_ELASTIC)
+	await tween.tween_property(panel, "scale", Vector2(1,1), 0.5)
+
 func _ready():
 	visible = false
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	
+
 func _input(event):
 	if event.is_action_pressed("pause_menu"):
 		toggle_pause()
@@ -12,8 +50,10 @@ func _input(event):
 func toggle_pause():
 	ManipuladorPause.pause_all()
 	visible = !visible
-		
-	
+	if get_tree().paused:
+		menu_pause()
+
+
 func _on_resume_pressed() -> void:
 	ManipuladorPause.pause_all()
 	visible = false
@@ -22,3 +62,20 @@ func _on_resume_pressed() -> void:
 func _on_back_main_menu_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
+
+
+func _on_options_pressed() -> void:
+	options()
+
+
+
+func _on_audio_pressed() -> void:
+	audio()
+
+
+func _on_controles_pressed() -> void:
+	controles()
+
+
+func _on_back_pressed() -> void:
+	menu_pause()
